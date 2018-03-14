816c304239facd60d046d141545c8efcc5f27e61
semiller10 <samuelmiller10@gmail.com>
2018-03-14 16:07:58 -0500

    Argument parser added

--- uid-to-hierarchy.py
+++ uid-to-hierarchy.py
@@ -1,2 +1,21 @@
+import ArgumentParser
+
+def main():
+
+    args = get_args()
+
+def get_args():
+    '''
+    Get command line arguments
+    '''
+
+    parser = ArgumentParser()
+    parser.add_argument('-u', '--uids', help='Path to DIAMOND NCBI Taxonomy id output file')
+    parser.add_argument('-o', '--out', help='Path to taxonomic hierarchy table output')
+
+    args = parser.parse_args()
+
+    return args
+
 if __name__ == "__main__":
     main()
