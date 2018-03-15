207204aafb5f6acea3f1942e7d7137ecb8e23b5b
semiller10 <samuelmiller10@gmail.com>
2018-03-15 01:16:45 -0500

    Debugged

--- uid-to-hierarchy.py
+++ uid-to-hierarchy.py
@@ -1,4 +1,4 @@
-import ArgumentParser
+import argparse
 from Bio import Entrez
 import pandas as pd
 import sys
@@ -9,7 +9,7 @@ from functools import partial
 from multiprocessing import current_process, Pool
 
 Entrez.email = 'samuelmiller10@gmail.com'
-target_ranks = set([
+target_ranks = [
     'species', 
     'genus', 
     'family', 
@@ -17,7 +17,13 @@ target_ranks = set([
     'class', 
     'phylum', 
     'superkingdom'
-])
+]
+superkingdoms=[
+    'Archaea', 
+    'Bacteria',
+    'Eukaryota',
+    'Viruses'    
+]
 out_cols = [
     'gene_callers_id', 
     't_phylum', 
@@ -42,33 +48,38 @@ def main():
     args = get_args()
     threads = args.threads
 
-    uid_df = pd.read_csv(args.uid, sep='\t', header=None, columns=['qid', 'uid', 'e'])
+    uid_df = pd.read_csv(args.uid, sep='\t', header=None, names=['qid', 'uid', 'e'])
     assert pd.api.types.is_numeric_dtype(uid_df['uid'])
-    # e-values should include "e" for exponentiation
-    assert pd.api.types.is_object_dtype(uid_df['e'])
-
+    assert pd.api.types.is_numeric_dtype(uid_df['e'])
+    
     uids = uid_df['uid'].tolist()
-    uniq_uids = list(set(uid_df['uid']))
+    uniq_uids = list(set(uids))
     # 0 is the value when no alignment is found
     uniq_uids.remove(0)
+    # "Cellular organisms"
+    uniq_uids.remove(131567)
     one_pct_tot = len(uniq_uids) / 100 / threads
     procedure = 'Taxonomic hierarchy recovery'
     rank_record = OrderedDict().fromkeys(target_ranks)
     mp_pool = Pool(threads)
-    uniq_hiers = mp_pool.map(get_hier, uniq_uids)
+    froz_get_hier = partial(get_hier, rank_record=rank_record)
+    uniq_hiers = mp_pool.map(froz_get_hier, uniq_uids)
     mp_pool.close()
     mp_pool.join()
 
     uid_hier_dict = OrderedDict().fromkeys(uniq_uids)
     uid_hier_dict[0] = ['' for rank in target_ranks]
+    uid_hier_dict[131567] = ['' for rank in target_ranks]
+    for i, uniq_uid in enumerate(uniq_uids):
+        uid_hier_dict[uniq_uid] = uniq_hiers[i]
     hiers = []
     for i, uid in enumerate(uids):
         hiers.append(uid_hier_dict[uid])
     hier_df = pd.DataFrame(hiers, columns=target_ranks)
-    out_df = pd.concat([uid_df[['qid']], hier_df[target_ranks[-1]]], axis=1)
-    out_df.drop('superkingdom', inplace=True)
+    out_df = pd.concat([uid_df[['qid']], hier_df[target_ranks[::-1]]], axis=1)
+    out_df.drop('superkingdom', axis=1, inplace=True)
     out_df.columns = out_cols
-    out_df.to_csv(args.out, sep='\t')
+    out_df.to_csv(args.out, sep='\t', index=False)
 
     return
 
@@ -87,7 +98,7 @@ def get_hier(uid, rank_record):
                 data = Entrez.read(Entrez.efetch(db='Taxonomy', id=uid))
                 break
             except:
-                print('Waiting for Entrez to process UID ', str(uid), flush=True)
+                print('Waiting for Entrez to process UID', str(uid), flush=True)
                 time.sleep(2)
         uid_taxon = data[0]['ScientificName']
         uid_rank = data[0]['Rank']
@@ -104,11 +115,12 @@ def get_hier(uid, rank_record):
                 rank_record[higher_rank] = entry['ScientificName']
                 hier_ranks.add(higher_rank)
         # Record unresolvable ranks
-        for rank in target_ranks.difference(hier_ranks):
-            rank_dict[rank] = ''
+        for rank in set(target_ranks).difference(hier_ranks):
+            rank_record[rank] = ''
 
-        hier = [rank for rank in rank_dict.values()]
+        hier = [rank for rank in rank_record.values()]
         if hier[-1] not in superkingdoms:
+            print(hier, flush=True)
             print(
                 'Querying Entrez again with UID: ', str(uid), ' due to corrupted return data', 
                 flush=True
@@ -117,8 +129,6 @@ def get_hier(uid, rank_record):
             break
 
     return hier
-
-def get_lineage(uid):
 
 def print_prog():
     '''
@@ -135,21 +145,25 @@ def print_prog():
                 print_over_line(procedure + ':' + str(pct) + '%')    
 
 def print_over_line(msg):
-    if config.verbose[0]:
-        # Clear to end of line
-        sys.stdout.write('\033[K')
-        sys.stdout.write(msg + '\r')
-        sys.stdout.flush()
+    # Clear to end of line
+    sys.stdout.write('\033[K')
+    sys.stdout.write(msg + '\r')
+    sys.stdout.flush()
 
 def get_args():
     '''
     Get command line arguments
     '''
 
-    parser = ArgumentParser()
-    parser.add_argument('-u', '--uid', help='Path to DIAMOND NCBI Taxonomy UID output file')
-    parser.add_argument('-o', '--out', help='Path to taxonomic hierarchy table output')
-    parser.add_argument('-t', '--thread', help='Number of threads to use')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('uid', help='Path to DIAMOND NCBI Taxonomy UID output file')
+    parser.add_argument('out', help='Path to taxonomic hierarchy table output')
+    parser.add_argument(
+        '--threads', 
+        default=1, 
+        type=int, 
+        help='Number of threads to use'
+    )
 
     args = parser.parse_args()
 
